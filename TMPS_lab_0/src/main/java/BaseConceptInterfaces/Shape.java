0bf77795485f71f96e4d0d00ca4ea55678e812e7
arturtugui <146761295+arturtugui@users.noreply.github.com>
2025-10-16 22:21:28 +0300

    Added lab 0 report

--- TMPS_lab_0/src/main/java/BaseConceptInterfaces/Shape.java
+++ TMPS_lab_0/src/main/java/BaseConceptInterfaces/Shape.java
@@ -1,19 +1,7 @@
 package BaseConceptInterfaces;
 
-/**
- * Interface representing a geometric shape.
- * Defines the basic properties all shapes must have.
- */
 public interface Shape {
-    /**
-     * Calculates the area of the shape.
-     * @return the area
-     */
     double getArea();
-    
-    /**
-     * Calculates the perimeter of the shape.
-     * @return the perimeter
-     */
+
     double getPerimeter();
 }
